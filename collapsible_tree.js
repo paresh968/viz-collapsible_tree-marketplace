b64f81b5baf7908b5c1a8a375130bab48925b486
paresh968 <141769760+paresh968@users.noreply.github.com>
2025-07-27 15:03:58 +0530

    Update collapsible_tree.js
    collapsible_tree

--- collapsible_tree.js
+++ collapsible_tree.js
@@ -1,7 +1,12 @@
 const dscc = window.dscc;
 
 function drawViz(data) {
-  const container = document.getElementById("viz");
+  let container = document.getElementById("viz");
+  if (!container) {
+    container = document.createElement("div");
+    container.id = "viz";
+    document.body.appendChild(container);
+  }
   container.innerHTML = "";
   const pre = document.createElement("pre");
   pre.textContent = JSON.stringify(data.tables.DEFAULT, null, 2);
