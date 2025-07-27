a6988a0d01af0455720f7ca9741de77a75fb917f
paresh968 <141769760+paresh968@users.noreply.github.com>
2025-07-27 13:14:45 +0530

    Update collapsible_tree.js
    New update Collapsible Tree JS

--- collapsible_tree.js
+++ collapsible_tree.js
@@ -2,14 +2,10 @@ const dscc = window.dscc;
 
 function drawViz(data) {
   const container = document.getElementById("viz");
-  container.innerHTML = ""; // clear previous
-
-  const sample = JSON.stringify(data.tables.DEFAULT, null, 2);
-
+  container.innerHTML = "";
   const pre = document.createElement("pre");
-  pre.textContent = sample;
+  pre.textContent = JSON.stringify(data.tables.DEFAULT, null, 2);
   container.appendChild(pre);
 }
 
-// This sets up Looker Studio's visualization subscription
 dscc.subscribeToData(drawViz, { transform: dscc.tableTransform });
